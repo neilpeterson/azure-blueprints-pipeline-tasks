8db4cfecca33e4c472759f8451a89b8fd34c1901
Neil Peterson <neilpeterson@outlook.com>
2019-07-02 14:52:16 -0700

    Updated create to use the new PowerShell module

--- createBlueprint/createBlueprint.ps1
+++ createBlueprint/createBlueprint.ps1
@@ -7,95 +7,64 @@
    Intent: Sample to demonstrate Azure BluePrints with Azure DevOps
 #>
 
-# Helper functions
-Import-Module ./helperFunctions.psm1
-
 # Get authentication details
 $ConnectedServiceName = Get-VstsInput -Name ConnectedServiceName -Require
 $Endpoint = Get-VstsEndpoint -Name $ConnectedServiceName -Require
 $TenantId = $Endpoint.Auth.Parameters.tenantid
 $ClientId = $Endpoint.Auth.Parameters.ServicePrincipalId
-$ClientSecret = $Endpoint.Auth.Parameters.ServicePrincipalKey
+$ClientSecret = $Endpoint.Auth.Parameters.ServicePrincipalKey | ConvertTo-SecureString -AsPlainText -Force
 
 # Get service connection scope (Management Group or Subscription).
 $ServiceConnectionScope = $Endpoint.Data.scopeLevel
 $BlueprintManagementGroup = $Endpoint.Data.managementGroupId
-$BlueprintSubscription = Get-VstsInput -Name AlternateLocation
-
-if ($BlueprintSubscription -eq "true") {
-   $SubscriptionID = Get-VstsInput -Name AlternateSubscription
-   $ServiceConnectionScope = "Subscription"
-} else {
-   $SubscriptionID = $Endpoint.Data.SubscriptionId
-}
+$BlueprintSubscriptionID = $Endpoint.Data.subscriptionId
+$BlueprintAltSubscription = Get-VstsInput -Name AlternateLocation
+$BlueprintAltSubscriptionID = Get-VstsInput -Name AlternateSubscription
 
 # Get blueprint details
 $BlueprintName = Get-VstsInput -Name BlueprintName
 $BlueprintPath = Get-VstsInput -Name BlueprintPath
-$ArtifactsPath = Get-VstsInput -Name ArtifactsPath
+$BlueprintPath = $env:SYSTEM_DEFAULTWORKINGDIRECTORY + $BlueprintPath
 $PublishBlueprint = Get-VstsInput -Name PublishBlueprint
 $BlueprintVersion = Get-VstsInput -Name Version
 
-# Get Blueprint and Artifact paths
-$BlueprintPath = $env:SYSTEM_DEFAULTWORKINGDIRECTORY + $BlueprintPath
-$ArtifactPath = $env:SYSTEM_DEFAULTWORKINGDIRECTORY + $ArtifactsPath
+# Install Azure PowerShell modules
+Find-Module Az.Accounts | Install-Module -Force
+Find-Module Az.Blueprint | Install-Module -Force
 
-# Get Access Token
-$Resource = "https://management.core.windows.net/"
-$RequestAccessTokenUri = 'https://login.microsoftonline.com/{0}/oauth2/token' -f $TenantId
-$Body = "grant_type=client_credentials&client_id={0}&client_secret={1}&resource={2}" -f $ClientId, $ClientSecret, $Resource
-$Token = Invoke-RestMethod -Method Post -Uri $RequestAccessTokenUri -Body $Body
-
-# Header for all REST calls
-$Headers = @{}
-$Headers.Add("Authorization","$($Token.token_type) "+ " " + "$($Token.access_token)")
-
-# Create / Update Blueprint
-$Body = Get-Content -Raw -Path $BlueprintPath
-$BlueprintURI = Get-BlueprintURI -Scope $ServiceConnectionScope -ManagementGroup $BlueprintManagementGroup -SubscriptionID $SubscriptionID -BlueprintName $BlueprintName
-Invoke-RestMethod -Method PUT -Uri $BlueprintURI -Headers $Headers -Body $Body -ContentType "application/json"
-
-# Remove exsisting artifacts
-$ArtifactsURI = Get-AllArtifactsURI -Scope $ServiceConnectionScope -ManagementGroup $BlueprintManagementGroup -SubscriptionID $SubscriptionID -BlueprintName $BlueprintName
-$artifacts = Invoke-RestMethod -Method GET -Uri $ArtifactsURI -Headers $Headers
-
-if ($artifacts.value.count -gt 0) {
-   foreach ($artifact in $artifacts) {
-      $ArtifactDeleteURI = Get-ArtifactURI -Scope $ServiceConnectionScope -ManagementGroup $BlueprintManagementGroup -SubscriptionID $SubscriptionID -BlueprintName $BlueprintName -ArtifactName $artifact.value.name
-      Invoke-RestMethod -Method DELETE -Uri $ArtifactDeleteURI -Headers $Headers
-  }
+# Set Blueprint Scope (Subscription / Management Group)
+if ($ServiceConnectionScope -eq 'ManagementGroup' -and $BlueprintAltSubscription -eq "false" ) {
+   $BlueprintScope = "-ManagementGroupId $BlueprintManagementGroup"
 }
 
-# Add new / updated artifacts
-if (Test-Path $ArtifactPath) {
-   $allArtifacts = Get-ChildItem $ArtifactPath
-   if ($allArtifacts.count -gt 0) {
-      foreach ($item in $allArtifacts) {
-         $Body = Get-Content -Raw -Path $item.FullName
-         $ArtifactCreateURI = Get-ArtifactURI -Scope $ServiceConnectionScope -ManagementGroup $BlueprintManagementGroup -SubscriptionID $SubscriptionID -BlueprintName $BlueprintName -ArtifactName $item.name.Split('.')[0]
-         Invoke-RestMethod -Method PUT -Uri $ArtifactCreateURI -Headers $Headers -Body $Body -ContentType "application/json"
-      }
-   }
+if ($ServiceConnectionScope -eq 'ManagementGroup' -and $BlueprintAltSubscription -eq "true" ) {
+   $BlueprintScope = "-SubscriptionId $BlueprintAltSubscriptionID"
 }
 
-# Publish blueprint
+if ($ServiceConnectionScope -eq 'Subscription') {
+   $BlueprintScope = "-SubscriptionId $BlueprintSubscriptionID"
+}
+
+# Connect to Azure
+$Creds = New-Object System.Management.Automation.PSCredential ($ClientId, $ClientSecret)
+Connect-AzAccount -ServicePrincipal -Tenant $TenantId -Credential $Creds -WarningAction silentlyContinue
+
+# Create Blueprint
+Invoke-Expression "Import-AzBlueprintWithArtifact -Name $BlueprintName -InputPath $BlueprintPath $BlueprintScope -Force"
+
+# Publish blueprint if publish
 if ($PublishBlueprint -eq "true") {
 
-    # Get current blueprint version or use pipeline string
+   # Set version if increment
    if ($BlueprintVersion -eq "Increment") {
-      $BlueprintVersionURI = Get-BlueprintVersionsURI -Scope $ServiceConnectionScope -ManagementGroup $BlueprintManagementGroup -SubscriptionID $SubscriptionID -BlueprintName $BlueprintName
-      $pubBP = Invoke-RestMethod -Method GET -Uri $BlueprintVersionURI -Headers $Headers
-
-      if (!$pubBP.value[$pubBP.value.Count - 1].name) {
-         $version = 1
-      } else {
-         $version = ([int]$pubBP.value[$pubBP.value.Count - 1].name) + 1
-      }
-   } else {
-      $version = $BlueprintVersion
+      $BlueprintObject = Invoke-Expression "Get-AzBlueprint -Name $BlueprintName $BlueprintScope"
+         if ($BlueprintObject.versions[$BlueprintObject.versions.count - 1] -eq 0) {
+            $BlueprintVersion = 1
+         } else {
+            $BlueprintVersion = ([int]$BlueprintObject.versions[$BlueprintObject.versions.count - 1]) + 1
+         }
    }
 
    # Publish blueprint
-   $PublishBlueprintURI = Get-PublishBlueprintURI -Scope $ServiceConnectionScope -ManagementGroup $BlueprintManagementGroup -SubscriptionID $SubscriptionID -BlueprintName $BlueprintName -BlueprintVersion $version
-   Invoke-RestMethod -Method PUT -Uri $PublishBlueprintURI -Headers $Headers
+   Publish-AzBlueprint -Blueprint $BluePrintObject -Version $BlueprintVersion
 }
