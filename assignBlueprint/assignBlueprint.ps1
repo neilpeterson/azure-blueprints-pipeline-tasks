fa585ffeb350462e91999a5c259a10d418ccf863
David Shorter <david.shorter@myfoodbag.co.nz>
2019-11-28 11:46:27 +1300

    Fail build

--- assignBlueprint/assignBlueprint.ps1
+++ assignBlueprint/assignBlueprint.ps1
@@ -103,7 +103,7 @@ if ($Wait -eq "true") {
         $AssignemntStatus = Get-AzBlueprintAssignment -Name $AssignmentName -SubscriptionId $TargetSubscriptionID
         if ($AssignemntStatus.ProvisioningState -eq "failed") {
             Write-Host "##vso[task.logissue type=error;]Assignment Failed, see Azure portal for results."
-            break
+            exit 1
         }
     }
 
